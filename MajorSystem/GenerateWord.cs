af4d16869de501945d1f84d40afb438750d0bf35
Magnus <magnus.axelsson@hotmail.com>
2018-05-10 23:02:28 +0200

    First commit.

---
+++ MajorSystem/GenerateWord.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MajorSystem
+{
+	public class Major
+	{
+		public void GenerateWord(int nummer)
+		{
+
+			List<string> vowels = new List<string>();
+
+			vowels.Add("a");
+			vowels.Add("e");
+			vowels.Add("i");
+			vowels.Add("o");
+			vowels.Add("u");
+			vowels.Add("y");
+			vowels.Add("å");
+			vowels.Add("ä");
+			vowels.Add("ö");
+
+
+
+		}
+	}
+}
